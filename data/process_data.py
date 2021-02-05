78f3d4b0249bd6916ebed7e8c18b1dda83a3227e
Rubén Darío Castillo <67809661+rudacaya@users.noreply.github.com>
2021-02-05 11:56:18 -0500

    ETL Pipeline Construction
    Load, clean and save data in a SQLite database

--- data/process_data.py
+++ data/process_data.py
@@ -1,15 +1,38 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    #Read files
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    #merge datasets
+    df = messages.merge(categories, how = 'inner', on = 'id')
+    return df
 
 
 def clean_data(df):
-    pass
-
-
+    categories = df.categories.str.split(';', expand = True)
+    #first row to extract category columns names
+    row = categories.loc[0,:]
+    category_colnames = row.apply(lambda x: x.split('-')[0])
+    categories.columns = category_colnames
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].apply(lambda x: x.split('-')[1])
+        
+        # convert column from string to numeric
+        categories[column] = pd.to_numeric(categories[column])
+    df1 = df.drop('categories', axis = 1).copy()
+    df1  = pd.concat([df1, categories], axis = 1) 
+    #drop duplicates
+    df1 = df1[~df1.duplicated()]
+    return df1
+    
 def save_data(df, database_filename):
+    engine = create_engine('sqlite:///InsertDatabaseName.db')
+    df.to_sql(database_filename, engine, index=False)
     pass  
 
 
